357a1fa87de51830aeb593efc8e6acc33b6159c8
20242412 <20242412@sdhs.gwe.hs.kr>
2024-11-22 15:11:32 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -122,7 +122,7 @@ content_data = {
             "https://www.youtube.com/watch?v=3JZ_D3ELwOQ"
         ],
         'texts': [
-            "Label 3 한국전투기기술력은세계제.",
+            "Label 3 한국전투기기술력은세계제일.",
             "Label 3 관련 두 번째 텍스트 내용입니다.",
             "Label 3 관련 세 번째 텍스트 내용입니다."
         ]
